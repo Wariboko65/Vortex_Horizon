f5d3baae897d62657f26aa91dcc687da60fe23f1
John Godwin Oamien <oamienjoh20034@gmail.com>
2023-09-06 10:37:49 -0700

    d

--- index.js
+++ index.js
@@ -89,5 +89,21 @@ function seeMoreFunction() {
 }
 seeMoreBttn.addEventListener("click", seeMoreFunction);
 
-// Testimonial Slides
+// Connect to Google Sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx9fMiZB8XSKZtHKJ1f9UMklkLlcH9NhfDXQrqmRpo9u7YKGnPSNnTrTvCPmEZD73ql/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
 
+form.addEventListener('submit', e => {
+e.preventDefault();
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+});
+// Connected to Google Sheet Successfully
