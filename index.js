503677dd55c265d7565d1ff46714da4f7d16014f
John Godwin Oamien <oamienjoh20034@gmail.com>
2023-09-17 08:03:03 -0700

    f

--- index.js
+++ index.js
@@ -194,6 +194,9 @@ function themeChange() {
     if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
         document.body.classList.remove("light");
         document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
     }
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
         if (e.matches) {
