5c8340deb2a8677787191e7c5476cb8dec73b5c0
John Godwin Oamien <oamienjoh20034@gmail.com>
2023-10-24 20:29:01 +0100

    h

--- index.js
+++ index.js
@@ -210,6 +210,8 @@ window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e)
     } else {
         light();
     }
+    const currentTheme = document.body.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", currentTheme);
 });
 
 window.addEventListener("load", () => {
@@ -233,3 +235,33 @@ window.addEventListener("load", () => {
 themeBtn.addEventListener("click", () => {
     themeChange();
 });
+
+
+const faders =  document.querySelectorAll(".fade-in");
+    const sliders = document.querySelectorAll(".slide-in");
+    
+    const appearOptions = {
+      threshold: 0,
+      rootMargin: "0px 0px -250px 0px"
+    };
+    
+    const appearOnScroll = new IntersectionObserver
+    (function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+        } else {
+          entry.target.classList.add("appear");
+          appearOnScroll.unobserve(entry.target);
+        }
+      }) 
+    },
+    appearOptions); 
+    
+    faders.forEach(fader => { 
+      appearOnScroll.observe(fader);
+    });
+     
+    sliders.forEach(slider => {
+      appearOnScroll.observe(slider);
+    });
