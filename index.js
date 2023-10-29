8dd8c22f5b44e8e9347ef1ef0969e5c1bc7ca11b
John Godwin Oamien <oamienjoh20034@gmail.com>
2023-10-29 20:02:20 +0100

    d

--- index.js
+++ index.js
@@ -94,15 +94,16 @@ const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-e.preventDefault();
-fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully";
-        setTimeout(function(){
-            msg.innerHTML = "";
-        }, 2000)
-        form.reset();
-    })
+    e.preventDefault();
+    formValidation();
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully";
+            setTimeout(function() {
+                msg.innerHTML = "";
+            }, 2000)
+            form.reset();
+        })
     .catch(error => console.error('Error!', error.message));
 });
 
@@ -133,8 +134,6 @@ function formValidation() {
         firstEmptyInput.focus();
         return;
     }
-
-    form.submit();
 }
 let cdbttn = document.querySelector("[class='contact-btn']");
 cdbttn.addEventListener("click", formValidation);
